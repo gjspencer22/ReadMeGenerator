7f2caa4db80f81ad6a4f1a96e5f32ade2f4db04f
gjspencer22 <gjspencer22@gmail.com>
2021-03-29 01:57:56 -0600

    right order

---
+++ README.md
@@ -0,0 +1,45 @@
+# Read Me Generator
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/gjspencer22/ReadMeGenerator?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/gjspencer22/ReadMeGenerator?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  
+  Using this, you can quickly generate a readme file by answering some questions so you don't have to waste time making a new one every project.
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  You must first run npm install in the terminal. You must also run "npm install inquirer" and "npm install axios" 
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  In the integrated terminal in vs code, run "node index.js" and then proceed to answer all of the questions that are asked.
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  N/A
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](https://avatars.githubusercontent.com/u/77038984?v=4) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@gjspencer22](https://api.github.com/users/gjspencer22)
+
+  Email: null
+  
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,116 @@
+function generateMarkdown(userResponses, userInfo) {
+
+  // Generate Table of Contents conditionally based on userResponses
+  let draftToC = `## Table of Contents`;
+
+  if (userResponses.installation !== '') { draftToC += `
+  * [Installation](#installation)` };
+
+  if (userResponses.usage !== '') { draftToC += `
+  * [Usage](#usage)` };
+
+  if (userResponses.contributing !== '') { draftToC += `
+  * [Contributing](#contributing)` };
+
+  if (userResponses.tests !== '') { draftToC += `
+  * [Tests](#tests)` };
+
+
+  // Generate markdown for the top required portions of the README
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  
+  ${userResponses.description}
+  `
+
+  // Add Table of Contents 
+  draftMarkdown += draftToC;
+ 
+  // installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  ${userResponses.installation}`
+  };
+  
+
+  //  Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  ${userResponses.usage}`
+  };
+  
+  
+  //  Contributing section
+  if (userResponses.contributing !== '') {
+  `
+  
+  ## Contributing
+  
+  *If you would like to contribute it, you can follow these guidelines for how to do so.*
+  
+  ${userResponses.contributing}`
+  };
+  
+
+  // Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  ${userResponses.tests}`
+  };
+
+
+
+  // Questions / About Developer section
+  let draftDev = 
+  `
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](${userInfo.avatar_url}) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@${userInfo.login}](${userInfo.url})
+
+  Email: ${userInfo.email}
+  `;
+
+  // Add developer section to markdown
+  draftMarkdown += draftDev;
+
+  // Return markdown
+  return draftMarkdown;
+  
+}
+
+module.exports = generateMarkdown;
